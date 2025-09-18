89dbadc43e87b1fc05a8dea90b78e1db27844b66
AbuTokio <bunyaminxkara@gmail.com>
2025-09-18 15:12:26 +0200

    added back and back to top button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,26 @@
+import { useNavigate } from "react-router"
+
 interface ButtonProps {
-  label: string
+  label?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined
+  backbutton?: boolean
+  toTop?: boolean
 }
 
-export default function Button({ label }: ButtonProps) {
+export default function Button({ label, onClick, backbutton = false, toTop = false }: ButtonProps) {
+  const navigate = useNavigate()
+
   return (
-    <button className="px-10 py-3 font-poppins-bold font-bold text-white bg-primary rounded-xl cursor-pointer hover:scale-105 active:scale-95">
-      {label}
+    <button
+      className={`${
+        label && "px-10 py-3 rounded-xl"
+      } font-poppins-bold font-bold text-white bg-primary cursor-pointer hover:scale-105 active:scale-95 ${
+        backbutton && "w-12 h-12 fixed top-5 left-5 rounded-full"
+      } ${toTop && "w-12 h-12 fixed bottom-5 right-5 rounded-full"}`}
+      onClick={
+        backbutton ? () => navigate(-1) : toTop ? () => window.scrollTo({ top: 0, behavior: "smooth" }) : onClick
+      }>
+      {backbutton ? "←" : toTop ? "↑" : label}
     </button>
   )
 }
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,42 @@
 import { Outlet } from "react-router"
 import Header from "../components/header/Header"
 import Footer from "../components/footer/Footer"
+import { useEffect, useRef, useState } from "react"
+import Button from "../components/button/Button"
 
 export default function Layout() {
+  const mainRef = useRef<HTMLElement>(null)
+  const [showToTopButton, setShowToTopButton] = useState(false)
+
+  useEffect(() => {
+    const mainElement = mainRef.current
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setShowToTopButton(entry.isIntersecting)
+      },
+      {
+        root: null,
+        threshold: 0,
+        rootMargin: "0px 0px -100% 0px",
+      }
+    )
+
+    if (mainElement) observer.observe(mainElement)
+
+    return () => {
+      if (mainElement) observer.unobserve(mainElement)
+    }
+  }, [])
+
   return (
     <>
       <Header />
-      <main className="px-46.5">
+      <main className="px-46.5" ref={mainRef}>
         <Outlet />
       </main>
       <Footer />
+      {showToTopButton && <Button toTop />}
     </>
   )
 }
