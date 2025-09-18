45f4630707facf3455a74a71f7bdfe1dfd5bb06e
BinaryBecca <rebecca.thinnes@web.de>
2025-09-18 13:14:23 +0200

    fetching categories finished

--- src/functions/Functions.ts
+++ src/functions/Functions.ts
@@ -2,12 +2,15 @@ import axios from "axios"
 import type { ICategories } from "../interfaces/ICategories"
 
 // #Categories Page
-export const categories = async (): Promise<ICategories> => {
+export const categories = async (): Promise<ICategories | null> => {
   try {
     const resp = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php")
-    console.log(resp)
-    if (resp.data.categories) {
-      return resp.data
-    }
-  } catch (error) {}
+    // console.log(resp)
+    // console.log(resp.data.categories)
+    if (resp.data.categories) return resp.data
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Kategorien", error)
+    return null
+  }
+  return null
 }
