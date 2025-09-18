3bfcc561da89c71a5dbdce943906d04e0957877a
m0lt <maltepfeiffer@web.de>
2025-09-18 13:21:08 +0200

    provider und fetch für search

---
+++ src/context/mealProvider.tsx
@@ -0,0 +1,48 @@
+import axios from "axios"
+import type { IMeal, IMeals } from "../interfaces/IMeals"
+import { createContext, useEffect, useState } from "react"
+
+export const mealContext = createContext<MealProviderProps | null>(null)
+
+export interface MealProviderProps {
+  searchTerm: string
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>
+  meals: IMeal[]
+  setMeals: React.Dispatch<React.SetStateAction<IMeal[]>>
+}
+
+export default function MealProvider({ children }: { children: React.ReactNode }) {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [meals, setMeals] = useState<IMeal[]>([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        if (!searchTerm) {
+          setMeals([])
+          return
+        }
+
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        const resp = await axios.get<IMeals>(url)
+        setMeals(resp.data.meals)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    getData()
+  }, [searchTerm])
+
+  return (
+    <mealContext.Provider
+      value={{
+        searchTerm,
+        setSearchTerm,
+        meals,
+        setMeals,
+      }}>
+      {children}
+    </mealContext.Provider>
+  )
+}
